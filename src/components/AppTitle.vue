a1da417d6595a5c96e4925e78e997e9c4ce3172c
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-06-28 16:32:05 +0200

    added AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "Iris nel giardino di Monet",
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Alla scoperta di - {{ title }}</h1>
+</template>
+
+<style scoped>
+    h1 {
+        color: rgb(196, 196, 122);
+    }
+</style>
